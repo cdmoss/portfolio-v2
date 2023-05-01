bc794298b27a038549e879f685299ab415c34fa0
chase <chase.mossing@uleth.ca>
2023-05-01 03:04:10 -0600

    added hex image, played around with drawing svgs

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -1,13 +1,37 @@
-import React, { useContext } from 'react';
-import { Variants, motion } from 'framer-motion';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { motion, useMotionValue, useMotionValueEvent } from 'framer-motion';
 import Image from 'next/image';
 import { ThemeContext } from '@/ThemeContext';
 import { FaChevronRight } from "react-icons/fa"
+import { ReactSVG } from 'react-svg';
+import styles from './Main.module.css'
+import { useScreenSize } from '@/hooks/useScreenSize';
+
+interface Coordinate {
+  x: number,
+  y: number
+}
 
 const DELAY_STEP = 0.25
 
+const PICTURE_SIZE = 500;
+
 const Main: React.FC = () => {
   const {theme} = useContext(ThemeContext)
+  const ulRef = useRef<HTMLUListElement | null>(null);
+  const picRef = useRef<HTMLImageElement | null>(null);
+
+  const {width, height} = useScreenSize()
+
+  // useMotionValueEvent(x, "animationComplete", () => {
+  //   if (ulRef.current && picRef.current) {
+  //     const ulBottom = { x: ulRef?.current?.getBoundingClientRect().left, y: ulRef?.current?.getBoundingClientRect().bottom };
+  //     const picBottom = { x: picRef?.current?.getBoundingClientRect().left, y: picRef?.current?.getBoundingClientRect().bottom };
+      
+  //     setUlConnectorPos(ulBottom)
+  //     setPicConnectorPos(picBottom)
+  //   }
+  // })
   
   // Define the bullet points
   const bulletPoints = [
@@ -37,12 +61,29 @@ const Main: React.FC = () => {
     },
   };
 
+  const draw = {
+    hidden: { pathLength: 0, opacity: 0 },
+    visible: (i: number) => {
+      const delay = 1 + i * 0.5;
+      return {
+        pathLength: 1,
+        opacity: 1,
+        transition: {
+          pathLength: { type: "spring", duration: 1.5, bounce: 0 },
+          opacity: { duration: 0.01 }
+        }
+      };
+    }
+  };
+
+  
 
   return (
-    <div className='flex h-full w-5/6 mx-auto'>
+    <div className='h-full w-full relative'>
+      <div className='flex h-full w-5/6 mx-auto'>
       {/* Left half with bullet points */}
-      <div className="flex-1 h-full flex flex-col relative items-center space-y-20">
-        <div className='flex h-full flex-col absolute top-1/4 px-10'>
+      <div className="flex-1 h-full flex flex-col items-center">
+        <div className='flex h-full flex-col absolute top-1/4'>
           <div style={{color: theme?.secondary}} className='text-6xl pb-10'>
             <span style={{color: theme?.accent}}>{"Hi, I'm "}</span>
             <span style={{color: theme?.secondary}}>{"Chase Mossing"}</span>
@@ -50,7 +91,7 @@ const Main: React.FC = () => {
           <p className='text-2xl mb-3' style={{color: theme?.accent}}>{"I'm a driven full-stack developer looking for opportunities to grow my skillset and build excellent software."}</p>
           <hr className='my-5'/>
           <p className='text-2xl mb-2' style={{color: theme?.secondary}}>{"Highlights:"}</p>
-          <ul>
+          <ul ref={ulRef}>
           {bulletPoints.map((point, index) => (
             <li className='my-3' key={index}>
               <motion.div
@@ -58,7 +99,8 @@ const Main: React.FC = () => {
                 variants={bulletVariants}
                 initial="hidden"
                 animate="visible"
-                className="flex justify-start">
+                className="flex justify-start"
+                style={{}}>
                   <FaChevronRight style={{color: theme?.secondary}} className='mr-5 mt-2' />
                   <span className='text-xl' style={{color: theme?.accent}}>{point}</span>
               </motion.div> 
@@ -68,25 +110,37 @@ const Main: React.FC = () => {
         </div>
       </div>
 
-      {/* Middle section */}
-      {/* <MaybeAnimateDiv hasRenderedBefore={hasRenderedBefore} variants={skillsVariants} className="flex-1 break-all">        
-          skillsskillsskillsskillsskillssk illsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskills skillsskillsskillsskillsskillsskillsskillsskillsskillsskillss killsskillsskil lsskillsski lsskillsskillsskillsski llsskillsskillsskillsskillsskillsskillsskill sskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskillsskills
-      </MaybeAnimateDiv> */}
-
-      {/* Right half with image */}
       <motion.div
         variants={imageVariants}
         initial="hidden"
         animate="visible"
-        className="flex flex-none justify-center items-center">
+        className={`${styles.hexagon} flex justify-center items-center h-full`} style={{height: PICTURE_SIZE + 25, width: PICTURE_SIZE + 25, backgroundColor: theme?.secondary}}>
         <Image
             src="/person.png" // Replace with your image URL
             alt="Large"
-            className="h-auto"
-            width={500}
-            height={500}
+            ref={picRef}
+            className={styles.hexagon}
+            width={PICTURE_SIZE}
+            height={PICTURE_SIZE}
           />
         </motion.div>
+    </div>
+      {/* {(width && height) && 
+      <motion.svg 
+        className="absolute top-0 left-0" 
+        width="100%" 
+        height="100%" 
+        viewBox={`0 0 100 100`} 
+        initial="hidden" 
+        animate="visible">
+        <motion.path
+          d={`M0 15 L0 5 L10 5`}
+          stroke="#00cc88"
+          strokeLinejoin={'round'}
+          strokeLinecap={'butt'}
+          variants={draw}
+          custom={2}/>
+      </motion.svg>} */}
     </div>
   );
 };
